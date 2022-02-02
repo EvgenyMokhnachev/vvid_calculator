ec7105b3aaa1c70a8abeecf58d771459f7061b76
Fullstack Developer <fullstack.dev@computools.com>
2022-02-02 12:09:36 +0300

    Добавил обработку вводиты данных - данные делятся на отображаемые символы и на символы управления; Добавил обработку символов "точка" и "запятая"; Добавил отчистку расчетов калькулятора; Добавил отчистку последнего отображаемого символа;

--- src/ru/sfedu/mohnachev/vvid/part2/calculator/CalculatorService.java
+++ src/ru/sfedu/mohnachev/vvid/part2/calculator/CalculatorService.java
@@ -7,6 +7,17 @@ public class CalculatorService {
     private final StringBuilder resultText = new StringBuilder("");
     private final List<CalculatorChangeResultListener> changeResultListeners = new ArrayList<>();
 
+    private String[] typeChars = new String[]{
+            "7", "8", "9",
+            "4", "5", "6",
+            "1", "2", "3",
+            ",", "0", "."
+    };
+
+    private String[] systemChars = new String[]{
+            "/", "*", "-", "=", "+", "C", "c", ""
+    };
+
     public void onChangeResultText(CalculatorChangeResultListener listener) {
         changeResultListeners.add(listener);
     }
@@ -18,34 +29,56 @@ public class CalculatorService {
     }
 
     public void typeValue(String value) {
-        if (validateTypedValue(value)) {
-            resultText.append(value);
+        for (String allowedChar : typeChars) {
+            if (allowedChar.equals(value)) {
+                processTypeValue(value);
+            }
+        }
+
+        for (String allowedChar : systemChars) {
+            if (allowedChar.equals(value)) {
+                processManageValue(value);
+            }
         }
         doChangeResultListeners();
     }
 
-    private String[] typeChars = new String[]{
-            "7", "8", "9",
-            "4", "5", "6",
-            "1", "2", "3",
-            ",", "0", "."
-    };
-    private String[] systemChars = new String[]{
-            "/", "*", "-", "=", "+", "C", "c", ""
-    };
+    private void processTypeValue(String value) {
+        if (value.equals(".")) value = ",";
+        if (value.equals(",")) {
+            if (resultText.length() == 0) {
+                resultText.append("0");
+            }
+            if (resultText.toString().contains(",")) {
+                return;
+            }
+        }
 
-    private boolean validateTypedValue(String value) {
-        for (String allowedChar : typeChars) {
-            if (allowedChar.equals(value)) {
-                return true;
-            }
+        resultText.append(value);
+
+        if (resultText.toString().equals("0")) {
+            resultText.append(",");
         }
-        for (String allowedChar : systemChars) {
-            if (allowedChar.equals(value)) {
-                return true;
-            }
+    }
+
+    private void processManageValue(String value) {
+        switch (value) {
+            case "c":;
+            case "C": clean(); break;
+            case "": cleanLastValue(); break;
         }
-        return false;
+    }
+
+    private void clean() {
+        if (resultText.length() > 0) {
+            resultText.delete(0, resultText.length());
+        }
+    }
+
+    private void cleanLastValue() {
+        if (resultText.length() > 0) {
+            resultText.delete(resultText.length() - 1, resultText.length());
+        }
     }
 
 }
